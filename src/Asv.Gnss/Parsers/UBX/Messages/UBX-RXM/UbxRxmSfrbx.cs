b0f20b4ead803915877e7db48cd819826a56d5a5
asv-soft-u2 <u02@asv.me>
2023-04-14 08:45:15 +0300

    Added gps and glonass raw ephemerides

---
+++ src/Asv.Gnss/Parsers/UBX/Messages/UBX-RXM/UbxRxmSfrbx.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using Asv.Common;
+
+namespace Asv.Gnss.UBX_RXM
+{
+    public class UbxRxmSfrbx : UbxMessageBase
+    {
+        public override string Name => "UBX-RXM-SFRBX";
+        public override byte Class => 0x02;
+        public override byte SubClass => 0x13;
+        public RxValue<bool> IsGps { get; set; }
+        public uint[] UIntBuffer { get; set; }
+        protected override void SerializeContent(ref Span<byte> buffer)
+        {
+            
+        }
+
+        protected override void DeserializeContent(ref ReadOnlySpan<byte> buffer)
+        {
+            UIntBuffer = new uint[buffer.Length];
+                
+            for (int i = 0; i < UIntBuffer.Length; i++)
+            {
+                UIntBuffer[i] = buffer[i];
+            }
+            
+            if (UIntBuffer[0] == 0)
+            {
+                IsGps.Value = true;
+                GpsSubFrame = GpsSubFrameFactory.Create(UIntBuffer);
+            }
+            else if (UIntBuffer[0] == 6)
+            {
+                IsGps.Value = false;
+                GlonassWord = GlonassWordFactory.Create(UIntBuffer);
+            }
+        }
+
+        protected override int GetContentByteSize() => UIntBuffer.Length;
+
+        public override void Randomize(Random random)
+        {
+            
+        }
+        
+        public GlonassWordBase GlonassWord { get; set; }
+        
+        public GpsSubframeBase GpsSubFrame { get; set; }
+    }
+}
