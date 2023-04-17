059ad78b56da1b73d098f6855fc87a2e0cbf197a
Alexey <248997@gmail.com>
2023-04-18 01:34:30 +0500

    Build bug fixed

--- build.bat
+++ build.bat
@@ -16,7 +16,7 @@ DEL version.txt
 rem build all projects
 (for %%p in (%projects%) do (
   	echo %%p
-	setversion %VERSION% ./src/%%p/%%p.csproj
+	setversion %VERSION:~1% ./src/%%p/%%p.csproj
 	dotnet restore ./src/%%p/%%p.csproj
 	dotnet build ./src/%%p/%%p.csproj -c Release
 	dotnet pack ./src/%%p/%%p.csproj -c Release
--- publish_nuget.bat
+++ publish_nuget.bat
@@ -12,8 +12,8 @@ DEL version.txt
 
 (for %%p in (%projects%) do (
 	cd src\%%p\bin\Release\
-	dotnet nuget push %%p.%VERSION%.nupkg --source https://api.nuget.org/v3/index.json
-	dotnet nuget push %%p.%VERSION%.nupkg --source https://nuget.pkg.github.com/asvol/index.json
+	dotnet nuget push %%p.%VERSION:~1%.nupkg --source https://api.nuget.org/v3/index.json
+	dotnet nuget push %%p.%VERSION:~1%.nupkg --source https://nuget.pkg.github.com/asvol/index.json
 	cd ../../../../
 )) 
 
